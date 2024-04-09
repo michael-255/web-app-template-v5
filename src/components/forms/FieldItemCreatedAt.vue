54d1374298c16cbb2bd20b299da0b2b9b6e96d4f
Michael Joy <kodiakchaos@gmail.com>
2024-04-09 16:21:21 -0400

    Cleaned up FieldItemCreatedAt component

--- src/components/forms/FieldItemCreatedAt.vue
+++ src/components/forms/FieldItemCreatedAt.vue
@@ -1,38 +1,29 @@
 <script setup lang="ts">
-import { calendarCheckIcon, calendarTodayIcon, scheduleTimeIcon } from '@/shared/icons'
+import { calendarTodayIcon, scheduleTimeIcon } from '@/shared/icons'
 import useSelectedStore from '@/stores/selected'
 import { date } from 'quasar'
-import { onMounted, ref, type Ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 const selectedStore = useSelectedStore()
 
-const existingTime: Ref<number | undefined> = ref(undefined)
-const displayDate = ref('')
+const displayDate = computed(() =>
+    date.formatDate(selectedStore.record.createdAt, 'ddd, YYYY MMM Do, h:mm A'),
+)
 const datePicker = ref('')
 const timePicker = ref('')
 
-onMounted(async () => {
-    existingTime.value = selectedStore.record.createdAt ?? Date.now()
-    datePicker.value = date.formatDate(existingTime.value, 'ddd MMM DD YYYY')
-    timePicker.value = date.formatDate(existingTime.value, 'HH:mm:00')
-    updateDisplayDate(existingTime.value)
+watch(
+    () => selectedStore.record.createdAt,
+    (newTimestamp) => {
+        datePicker.value = date.formatDate(newTimestamp, 'ddd MMM DD YYYY')
+        timePicker.value = date.formatDate(newTimestamp, 'HH:mm:00')
+    },
+)
+
+watch([datePicker, timePicker], () => {
+    const updatedTimestamp = new Date(`${datePicker.value} ${timePicker.value}`).getTime()
+    selectedStore.record.createdAt = updatedTimestamp
 })
-
-function updateDisplayDate(timestamp: number = Date.now()) {
-    selectedStore.record.createdAt = timestamp
-    displayDate.value = date.formatDate(timestamp, 'ddd, YYYY MMM Do, h:mm A')
-}
-
-function onPickerUpdate() {
-    // Set empty pickers with current time
-    datePicker.value = datePicker.value
-        ? datePicker.value
-        : date.formatDate(Date.now(), 'ddd MMM DD YYYY')
-    timePicker.value = timePicker.value ? timePicker.value : date.formatDate(Date.now(), 'HH:mm:00')
-
-    const dateTimestamp = new Date(`${datePicker.value} ${timePicker.value}`).getTime()
-    updateDisplayDate(dateTimestamp)
-}
 </script>
 
 <template>
@@ -42,61 +33,26 @@ function onPickerUpdate() {
 
             <q-item-label>Date and time the record was created.</q-item-label>
 
+            <q-item-label class="text-h6">{{ displayDate }}</q-item-label>
+
             <q-item-label caption>
-                <q-input
-                    v-model="displayDate"
-                    dense
-                    outlined
-                    disable
-                    color="primary"
-                    hint="Auto formatted"
-                >
-                    <template v-slot:after>
-                        <!-- Date Picker -->
-                        <q-btn :icon="calendarTodayIcon" color="primary" class="q-px-sm">
-                            <q-popup-proxy>
-                                <q-date v-model="datePicker" mask="ddd MMM DD YYYY">
-                                    <div class="row items-center justify-end q-gutter-sm">
-                                        <q-btn label="Cancel" flat v-close-popup />
-                                        <q-btn
-                                            label="OK"
-                                            color="primary"
-                                            flat
-                                            @click="onPickerUpdate()"
-                                            v-close-popup
-                                        />
-                                    </div>
-                                </q-date>
-                            </q-popup-proxy>
-                        </q-btn>
+                <!-- Date Picker -->
+                <q-btn :icon="calendarTodayIcon" color="primary" class="q-px-sm">
+                    <q-popup-proxy>
+                        <q-date v-model="datePicker" mask="ddd MMM DD YYYY" today-btn no-unset>
+                            <q-btn label="OK" color="primary" class="full-width" v-close-popup />
+                        </q-date>
+                    </q-popup-proxy>
+                </q-btn>
 
-                        <!-- Time Picker -->
-                        <q-btn :icon="scheduleTimeIcon" color="primary" class="q-ml-sm q-px-sm">
-                            <q-popup-proxy>
-                                <q-time v-model="timePicker" mask="HH:mm:00" now-btn>
-                                    <div class="row items-center justify-end q-gutter-sm">
-                                        <q-btn label="Cancel" flat v-close-popup />
-                                        <q-btn
-                                            label="OK"
-                                            color="primary"
-                                            flat
-                                            @click="onPickerUpdate()"
-                                            v-close-popup
-                                        />
-                                    </div>
-                                </q-time>
-                            </q-popup-proxy>
-                        </q-btn>
-
-                        <!-- Set DateTime to now -->
-                        <q-btn
-                            :icon="calendarCheckIcon"
-                            color="positive"
-                            class="q-ml-sm q-px-sm"
-                            @click="updateDisplayDate()"
-                        />
-                    </template>
-                </q-input>
+                <!-- Time Picker -->
+                <q-btn :icon="scheduleTimeIcon" color="primary" class="q-ml-sm q-px-sm">
+                    <q-popup-proxy>
+                        <q-time v-model="timePicker" mask="HH:mm:00" now-btn>
+                            <q-btn label="OK" color="primary" class="full-width" v-close-popup />
+                        </q-time>
+                    </q-popup-proxy>
+                </q-btn>
             </q-item-label>
         </q-item-section>
     </q-item>
