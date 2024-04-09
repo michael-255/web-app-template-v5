8dc14bc74ed43650e74fa47f080a3bd54710a6a6
Michael Joy <kodiakchaos@gmail.com>
2024-04-09 15:29:41 -0400

    Edit and Create page error logging

--- src/pages/PageCreate.vue
+++ src/pages/PageCreate.vue
@@ -3,6 +3,7 @@ import BaseMutationForm from '@/components/forms/BaseMutationForm.vue'
 import FabGoBack from '@/components/shared/FabGoBack.vue'
 import PageHeading from '@/components/shared/PageHeading.vue'
 import ResponsivePage from '@/components/shared/ResponsivePage.vue'
+import useLogger from '@/composables/useLogger'
 import useRouting from '@/composables/useRouting'
 import Example from '@/models/Example'
 import ExampleResult from '@/models/ExampleResult'
@@ -18,12 +19,17 @@ useMeta({ title: `${appName} - Create` })
 
 const { routeTable } = useRouting()
 const selectedStore = useSelectedStore()
+const { log } = useLogger()
 
 onMounted(async () => {
+    // TODO: Handle a parentId that has no record (do parentId stuff here?)
+
     if (routeTable === DBTableEnum.EXAMPLES) {
         selectedStore.record = new Example()
     } else if (routeTable === DBTableEnum.EXAMPLE_RESULTS) {
         selectedStore.record = new ExampleResult()
+    } else {
+        log.error('Create not supported on type', { routeTable })
     }
 })
 </script>
--- src/pages/PageEdit.vue
+++ src/pages/PageEdit.vue
@@ -3,6 +3,7 @@ import BaseMutationForm from '@/components/forms/BaseMutationForm.vue'
 import FabGoBack from '@/components/shared/FabGoBack.vue'
 import PageHeading from '@/components/shared/PageHeading.vue'
 import ResponsivePage from '@/components/shared/ResponsivePage.vue'
+import useLogger from '@/composables/useLogger'
 import useRouting from '@/composables/useRouting'
 import DB from '@/services/Database'
 import { appName } from '@/shared/constants'
@@ -10,7 +11,6 @@ import { DBTableEnum } from '@/shared/enums'
 import { editIcon } from '@/shared/icons'
 import type { UUIDType } from '@/shared/types'
 import { getTableLabel } from '@/shared/utils'
-import useLiveStore from '@/stores/live'
 import useSelectedStore from '@/stores/selected'
 import { extend, useMeta } from 'quasar'
 import { onMounted } from 'vue'
@@ -18,16 +18,23 @@ import { onMounted } from 'vue'
 useMeta({ title: `${appName} - Edit` })
 
 const { routeTable, routeId } = useRouting()
-const liveStore = useLiveStore()
 const selectedStore = useSelectedStore()
+const { log } = useLogger()
 
 onMounted(async () => {
+    // Must directly get records from the DB versus Store to avoid issues on reload
     if (routeTable === DBTableEnum.EXAMPLES) {
-        const example = liveStore.examples.find((example) => example.id === routeId)
+        const example = await DB.getExample(routeId as UUIDType)
         selectedStore.record = extend(true, {}, example) // Deep copy
     } else if (routeTable === DBTableEnum.EXAMPLE_RESULTS) {
         const exampleResult = await DB.getExampleResult(routeId as UUIDType)
         selectedStore.record = extend(true, {}, exampleResult) // Deep copy
+    } else {
+        log.error('Edit not supported on type', { routeTable })
+    }
+
+    if (Object.keys(selectedStore.record).length === 0) {
+        log.error('Record not found', { routeTable, routeId })
     }
 })
 </script>
