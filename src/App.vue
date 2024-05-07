a2e08c79fef4a805a132d3e5e67caef2be0f4a3d
Michael Joy <kodiakchaos@gmail.com>
2024-05-07 18:19:12 -0400

    link manifest

--- src/App.vue
+++ src/App.vue
@@ -24,29 +24,29 @@ useMeta({
         themeColor: { name: 'theme-color', content: `${colors.getPaletteColor('primary')}` },
     },
 
-    // link: {
-    //     manifest: {
-    //         rel: 'manifest',
-    //         href: `${import.meta.env.BASE_URL}manifest.json`,
-    //     },
-    //     appleTouchIcon: {
-    //         rel: 'apple-touch-icon',
-    //         sizes: '180x180',
-    //         href: `${import.meta.env.BASE_URL}apple-touch-icon.png`,
-    //     },
-    //     favicon32: {
-    //         rel: 'icon',
-    //         type: 'image/png',
-    //         sizes: '32x32',
-    //         href: `${import.meta.env.BASE_URL}favicon-32x32.png`,
-    //     },
-    //     favicon16: {
-    //         rel: 'icon',
-    //         type: 'image/png',
-    //         sizes: '16x16',
-    //         href: `${import.meta.env.BASE_URL}favicon-16x16.png`,
-    //     },
-    // },
+    link: {
+        manifest: {
+            rel: 'manifest',
+            href: `${import.meta.env.BASE_URL}manifest.json`,
+        },
+        // appleTouchIcon: {
+        //     rel: 'apple-touch-icon',
+        //     sizes: '180x180',
+        //     href: `${import.meta.env.BASE_URL}apple-touch-icon.png`,
+        // },
+        // favicon32: {
+        //     rel: 'icon',
+        //     type: 'image/png',
+        //     sizes: '32x32',
+        //     href: `${import.meta.env.BASE_URL}favicon-32x32.png`,
+        // },
+        // favicon16: {
+        //     rel: 'icon',
+        //     type: 'image/png',
+        //     sizes: '16x16',
+        //     href: `${import.meta.env.BASE_URL}favicon-16x16.png`,
+        // },
+    },
 
     noscript: {
         default:
